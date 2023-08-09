e156171d66462b0111ce71d0d79e69d338f9568e
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-08-09 11:54:38 +0000

    [add] plugin and host access

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :mystores, only: %i(index show new create edit update) do
+    resources :menus
+  end
+
+  resources :stores, only: [] do
+    resources :reviews, only: %i(index show new create)
+  end
+
+  resources :reviews, only: [] do
+    resources :comments, only: %i(create)
+    resource :favorites, only: %i(create destroy)
+    collection do
+      get :history
+    end
+  end
 end
