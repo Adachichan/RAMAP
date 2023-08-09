ab1f7d4c56c648759ae40f607162cd067545a002
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-08-09 13:58:13 +0000

    [add] #10 Devise_setting Devise関係の設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :mystores, only: %i(index show new create edit update) do
-    resources :menus
-  end
 
-  resources :stores, only: [] do
-    resources :reviews, only: %i(index show new create)
-  end
+  #---------------------------------------------------------------------------------------
+  # public routes
 
-  resources :reviews, only: [] do
-    resources :comments, only: %i(create)
-    resource :favorites, only: %i(create destroy)
-    collection do
-      get :history
-    end
-  end
+  # URL：/users/sign_in
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  #---------------------------------------------------------------------------------------
+  # admin routes
+
+  # URL：/admin/sign_in
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
 end
