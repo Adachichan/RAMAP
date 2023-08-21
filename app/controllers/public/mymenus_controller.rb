7e28ef107c4768d1b39dc8b54966f33995bca7d3
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-08-21 14:19:48 +0000

    [add] #31 mymenu_session 店舗ごとのメニュー登録・一覧・編集（更新）・削除（1つずつ、全部）の実装 ※レイアウト部分は未実装、form_withのurlは変数展開にて記載（editのview）

--- app/controllers/public/mymenus_controller.rb
+++ app/controllers/public/mymenus_controller.rb
@@ -1,7 +1,63 @@
 class Public::MymenusController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :identify_mymenu, only: [:edit, :update, :destroy]
+
   def index
+    @mystore = Store.find(params[:mystore_id])
+    @mymenu = Menu.new
+    # 店舗ごとの全メニューを取得
+    @mymenus = @mystore.menus.all
+    @number = 1
+  end
+
+  def create
+    @mystore = Store.find(params[:mystore_id])
+    @mymenu = Menu.new(mymenu_params)
+    @mymenu.store_id = params[:mystore_id]
+    # 店舗ジャンルの保存可否
+    if @mymenu.save
+      redirect_to mystore_mymenus_path(@mystore.id)
+    else
+      @mymenus = Menu.where(store_id:params[:mystore_id])
+      render :index
+    end
   end
 
   def edit
   end
+
+  def update
+    # メニュー情報の更新可否
+    if @mymenu.update(mymenu_params)
+      redirect_to mystore_mymenus_path(@mystore.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @mymenu
+      @mymenu.destroy
+    end
+    redirect_to mystore_mymenus_path(@mystore.id)
+  end
+
+  def destroy_all
+    @mystore = Store.find(params[:mystore_id])
+    @mystore.menus.destroy_all
+    redirect_to mystore_mymenus_path(@mystore.id)
+  end
+
+  private
+
+  def mymenu_params
+    params.require(:menu).permit(:menu_image, :name, :price)
+  end
+
+  def identify_mymenu
+    @mystore = Store.find(params[:mystore_id])
+    @mymenu = Menu.find(params[:id])
+  end
+
 end
