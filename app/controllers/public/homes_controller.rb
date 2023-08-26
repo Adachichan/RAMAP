ce01e3525b9ac3e0244df5550d8325242d47a581
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-08-26 14:30:20 +0000

    [add] #39 public_top_page_is_closed_narrow_down 閉店店舗をTOPページに表示させない

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @stores = Store.all
+    # 閉店した店舗は取り出し対象外
+    @stores = Store.where(is_closed: false)
     @stores_json = @stores.map{|o| { id: o.id, lat: o.latitude, lng: o.longitude, name: o.name } }.to_json
   end
 
