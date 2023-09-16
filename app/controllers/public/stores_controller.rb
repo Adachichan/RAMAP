e943ae60e86b9396cad1ccd1a10433a5b941615b
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-09-16 17:50:03 +0000

    [add] #68 public_store_show_layout ユーザーの店舗詳細画面（誰でも閲覧可能）のレイアウト実装

--- app/controllers/public/stores_controller.rb
+++ app/controllers/public/stores_controller.rb
@@ -2,7 +2,7 @@ class Public::StoresController < ApplicationController
 
   def show
     @store = Store.find(params[:id])
-    @menus = @store.menus
+    @menus = @store.menus.order(created_at: :desc).first(6)
   end
 
 end
