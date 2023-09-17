ee7b98a83aa91c74adadfca66dab8d8ec3eb23fa
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-09-17 01:58:34 +0000

    [middle] #70 public_menu_index_layout メニュー表示画面のレイアウト実装（途中）

--- app/controllers/public/menus_controller.rb
+++ app/controllers/public/menus_controller.rb
@@ -2,7 +2,8 @@ class Public::MenusController < ApplicationController
 
   def index
     @store = Store.find(params[:store_id])
-    @menus = @store.menus
+    @menus = @store.menus.page(params[:page]).per(12)
+    @menus_count = @store.menus.count
   end
 
 end
